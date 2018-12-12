10e71f0510a9c32d3a1e64f676cc9ea696d9274a
Carlos Johnny Noel Bernaola <cnoel@belatrix.com>
2018-12-12 11:44:27 -0500

    Adding Router. WO click, just URL typing.

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{
+    path: '/',
+    component: HomePage,
+  }, {
+    path: '/build',
+    name: 'Build',
+    component: RobotBuilder,
+  }],
+});
