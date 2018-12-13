7cbf578cc7c4e0ff4da4b19e20cedd196342a452
Carlos Johnny Noel Bernaola <cnoel@belatrix.com>
2018-12-13 10:05:27 -0500

    Prevent Leveing the Page wo added to cart. Navigation Guards

--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,28 @@ import RoboteHeads from '../parts/RobotHeads.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
+
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [{
     path: '/',
     name: 'Home',
-    component: HomePage,
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
   }, {
     path: '/build',
     name: 'Build',
-    component: RobotBuilder,
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
   },
   {
     path: '/parts/browse',
@@ -53,5 +63,9 @@ export default new Router({
     name: 'Parts',
     component: Partinfo,
     props: true,
+    beforeEnter(to, from, next) {
+      const isValidId = Number.isInteger(Number(to.params.id));
+      next(isValidId);
+    },
   }],
 });
